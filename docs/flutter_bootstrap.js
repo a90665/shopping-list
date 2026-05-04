1f669cec11e7ad5564bc022a6bda04c12f973a1f
a90665 <a90665@hotmail.com>
2026-05-04 14:09:31 +0800

    Remove boot logs and optimize mobile behavior

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "3327944075" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "2106943019" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
