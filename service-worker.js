801587bec04eeab31e183c05c344fb49be52f89a
a90665 <a90665@hotmail.com>
2026-07-31 11:20:29 +0800

    Deploy updated web app

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+﻿const CACHE_VERSION = "shopping-list-flet-v" + Date.now();
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
